ffee8b6ff4541afa9ce903af75bbd17837688f1f
Renata Monteiro <108954513+renataam@users.noreply.github.com>
2022-10-06 19:31:58 -0300

    axios api e JS temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+}
+
+let apiKey = "170ed67c56f7d3751961a6f26123ed61";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
 
