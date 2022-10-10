310265384563a4db01d24352c9dbd0e96f968018
Renata Monteiro <108954513+renataam@users.noreply.github.com>
2022-10-10 14:18:34 -0300

    previsão dos 5 dias em HTML CSS e JS

--- src/app.js
+++ src/app.js
@@ -22,6 +22,36 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(){
+  let forecastElement = document.querySelector("#forecast");
+  
+  let forecastHTML = `<div class="row">`;
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+        <div class="col-2">
+          <div class="weather-forecast-date">${day}</div>
+          <img 
+          src="https://openweathermap.org/img/wn/04d@2x.png" 
+          alt=""
+          width="42" 
+          />
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max">18º</span> 
+            <span class="weather-forecast-temperature-min">12º</span>
+          </div>
+        </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -85,3 +115,6 @@ let celsius = document.querySelector("#celsius");
 celsius.addEventListener("click", showcelsiusTemperature);
 
 search("Rio de Janeiro");
+
+displayForecast();
+
